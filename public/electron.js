bcbe70f320d179c693fe3a83d45fd9a528401939
CianOShea <cianoshea789@gmail.com>
2021-07-15 12:20:42 +0100

    Upgraded version to 0.1.3 and added Update Interval Check

--- public/electron.js
+++ public/electron.js
@@ -54,13 +54,13 @@ function createWindow() {
 
 }
 
-// setInterval(() => {
-//   autoUpdater.checkForUpdates()
-//   console.log('checking');
-//   mainWindow.webContents.on('did-finish-load', () => {
-//     mainWindow.webContents.send('version', app.getVersion())
-//   })
-// }, 60000)
+setInterval(() => {
+  autoUpdater.checkForUpdatesAndNotify();
+  console.log('checking');
+  mainWindow.webContents.on('did-finish-load', () => {
+    mainWindow.webContents.send('version', app.getVersion())
+  })
+}, 60000)
 
 // once electron app is ready, createWindow
 app.on('ready', () => {
@@ -80,24 +80,24 @@ ipcMain.on('test', (event, arg) => {
   mainWindow.send('test-back', 'pong')
 })
 
-// autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-//   const dialogOpts = {
-//     type: 'info',
-//     buttons: ['Restart', 'Later'],
-//     title: 'Application Update',
-//     message: process.platform === 'win32' ? releaseNotes : releaseName,
-//     detail: 'A new version has been downloaded. Restart the application to apply the updates.'
-//   }
+autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['Restart', 'Later'],
+    title: 'Application Update',
+    message: process.platform === 'win32' ? releaseNotes : releaseName,
+    detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+  }
 
-//   dialog.showMessageBox(dialogOpts).then((returnValue) => {
-//     if (returnValue.response === 0) autoUpdater.quitAndInstall()
-//   })
-// })
+  dialog.showMessageBox(dialogOpts).then((returnValue) => {
+    if (returnValue.response === 0) autoUpdater.quitAndInstall()
+  })
+})
 
-// autoUpdater.on('error', message => {
-//   console.error('There was a problem updating the application')
-//   console.error(message)
-// })
+autoUpdater.on('error', message => {
+  console.error('There was a problem updating the application')
+  console.error(message)
+})
 
 autoUpdater.on('checking-for-update', () => {
   console.log('Checking for update...');
@@ -108,15 +108,15 @@ autoUpdater.on('update-available', (info) => {
 autoUpdater.on('update-not-available', (info) => {
   console.log('Update not available.');
 })
-autoUpdater.on('error', (err) => {
-  console.log('Error in auto-updater. ' + err);
-})
+// autoUpdater.on('error', (err) => {
+//   console.log('Error in auto-updater. ' + err);
+// })
 autoUpdater.on('download-progress', (progressObj) => {
   let log_message = "Download speed: " + progressObj.bytesPerSecond;
   log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
   log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
   console.log(log_message);
 })
-autoUpdater.on('update-downloaded', (info) => {
-  console.log('Update downloaded');
-});
+// autoUpdater.on('update-downloaded', (info) => {
+//   console.log('Update downloaded');
+// });
